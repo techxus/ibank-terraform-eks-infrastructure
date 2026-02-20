c5f0443ee83e1280a3ee3e231bf08672d2b0424e
softnet <daniel@softnet.cloud>
2026-02-20 16:49:19 -0500

    fixes

--- envs/aws/dev/eks/main.tf
+++ envs/aws/dev/eks/main.tf
@@ -100,7 +100,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
+    host  = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    token = data.aws_eks_cluster_auth.this.token
   }
+}
